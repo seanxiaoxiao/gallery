ae60a6b1f953abd218f8f944247162469ccbd513
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-08-18 12:16:27 -0700

    Authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
-  before_filter :authenticate_user!
+  #before_filter :authenticate_user!
 
   def index
+    @data = APP_CONFIG['auth_users']
   end
 end
