81a7bbc43ed89e132c24da243940e4a1671ac67b
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-08-17 18:25:24 -0700

    Home slider finished

---
+++ app/assets/javascripts/home.js
@@ -0,0 +1,26 @@
+//# Place all the behaviors and hooks related to the matching controller here.
+//# All this logic will automatically be available in application.js.
+//# You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
+$(function(){
+    $('#slides').slides({
+        preload: true,
+        play: 5000,
+        pause: 2500,
+        hoverPause: true,
+        animationStart: function(current){
+        $('.caption').animate({
+            bottom:-35
+        },100);
+    },
+    animationComplete: function(current){
+                           $('.caption').animate({
+                               bottom:0
+                           },200);
+                       },
+    slidesLoaded: function() {
+                      $('.caption').animate({
+                          bottom:0
+                      },200);
+                  }
+    });
+});
