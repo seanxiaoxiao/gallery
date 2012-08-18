75e579fb8f69e780277c7c1bcc9040dc365631ec
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-08-17 18:44:57 -0700

    Add precompiled css file tags

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,9 @@ module Gallery
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Precompiled Css
+    config.assets.precompile += ['home.css']
+
   end
 end
