f81a165869844d9f4fad7186cf05bc75a2d0f34b
seanxiaoxiao <seanxiaoxiao@hotmail.com>
2012-08-19 10:50:57 -0700

    All prepared with cancan

--- app/models/photo.rb
+++
@@ -1,4 +0,0 @@
-class Photo < ActiveRecord::Base
-  attr_accessible :image_file_name, :name, :image
-  has_attached_file :image
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,26 @@
 class User < ActiveRecord::Base
+  ROLES = %w[admin moderator author normal guest banned]
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :roles
   # attr_accessible :title, :body
+  def roles=(roles)
+    self.roles_mask = (roles & ROLES).map { |r| 2**ROLES.index(r) }.inject(0, :+)
+  end
+
+  def roles
+    ROLES.reject do |r|
+      ((roles_mask || 0) & 2**ROLES.index(r)).zero?
+    end
+  end
+
+  def is?(role)
+    roles.include?(role.to_s)
+  end
+
 end
